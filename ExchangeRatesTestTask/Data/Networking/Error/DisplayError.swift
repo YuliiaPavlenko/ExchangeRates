9b6eeebc656f1cfe946712ec840a998bce67c73a
Yuliia Pavlenko <>
2020-04-17 19:03:59 +0200

    Add alert for error displaying

---
+++ ExchangeRatesTestTask/Data/Networking/Error/DisplayError.swift
@@ -0,0 +1,57 @@
+//
+//  DisplayError.swift
+//  ExchangeRatesTestTask
+//
+//  Created by Yuliia Pavlenko on 17/04/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import Foundation
+
+struct DisplayErrorModel {
+    var title: String
+    var message: String
+}
+
+enum DisplayError {
+    case currencyList, rates
+
+    func displayMessage(erError: ERError) -> DisplayErrorModel {
+        switch self {
+        case .currencyList:
+            return getErrorMessageForCurrencyLists(erError: erError)
+        case .rates:
+            return getErrorMessageGetRatesforDates(erError: erError)
+        }
+    }
+
+    private func getErrorMessageForCurrencyLists(erError: ERError) -> DisplayErrorModel {
+        let errorDescription = "Error get currencies list"
+        var message: String?
+        switch erError {
+        case .serverError, .unknown, .communicationError:
+            message = "Failed to currencies list. Please make sure you\'re connected to the internet and try again.\nContact support if the problem continues."
+        default:
+            message = "Unknown error"
+        }
+        #if DEBUG
+        message = erError.debugDescription
+        #endif
+        return DisplayErrorModel(title: errorDescription, message: message!)
+    }
+
+    private func getErrorMessageGetRatesforDates(erError: ERError) -> DisplayErrorModel {
+        let errorDescription = "Error get rates"
+        var message: String?
+        switch erError {
+        case .serverError, .unknown, .communicationError:
+            message = "Failed to get rates for selected dates. Please make sure you\'re connected to the internet and try again.\nContact support if the problem continues."
+        default:
+            message = "Unknown error"
+        }
+        #if DEBUG
+        message = erError.debugDescription
+        #endif
+        return DisplayErrorModel(title: errorDescription, message: message!)
+    }
+}
---
+++ ExchangeRatesTestTask/UI/Common/CustomErrorAlert.swift
@@ -0,0 +1,20 @@
+//
+//  CustomErrorAlert.swift
+//  ExchangeRatesTestTask
+//
+//  Created by Yuliia Pavlenko on 17/04/2020.
+//  Copyright © 2020 Yuliia Pavlenko. All rights reserved.
+//
+
+import UIKit
+
+class CustomErrorAlert {
+
+    static func setUpErrorAlert(_ withMessage: DisplayErrorModel?) -> UIAlertController {
+        let errorTitle = withMessage?.title
+        let alert = UIAlertController(title: errorTitle, message: withMessage?.message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (_: UIAlertAction!) in
+        }))
+        return alert
+    }
+}
