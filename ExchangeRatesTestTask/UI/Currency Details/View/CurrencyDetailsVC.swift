3637949edc488787329fee8eda981bd746622427
Yuliia Pavlenko <>
2020-04-18 11:37:33 +0200

    Add start / end date pickers

--- ExchangeRatesTestTask/UI/Currency Details/View/CurrencyDetailsVC.swift
+++ ExchangeRatesTestTask/UI/Currency Details/View/CurrencyDetailsVC.swift
@@ -9,16 +9,75 @@
 import UIKit
 
 class CurrencyDetailsVC: UIViewController {
+    
+    let startDateTextField = CurrencyDetailsViewElements.createDateTextField()
+    let endDateTextField = CurrencyDetailsViewElements.createDateTextField()
+    let startDatePicker = UIDatePicker()
+    let endDatePicker = UIDatePicker()
 
     var currencyDetailsPresenter = CurrencyDetailsPresenter()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        view.backgroundColor = Colors.white
         customizeNavigationBar(true)
 //        currencyDetailsPresenter.viewIsPrepared()
+        
+        configureDatePicker()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureDateTextFields()
     }
     
     func customizeNavigationBar(_ animated: Bool) {
         title = currencyDetailsPresenter.selectedCurrencyRate?.currency?.capitalized
     }
+    
+    func configureDateTextFields() {
+        startDateTextField.placeholder = "Enter start date"
+        endDateTextField.placeholder = "Enter end date"
+        
+        let datesStackView = CurrencyDetailsViewElements.createHorizontalStackView(arrangedSubviews: [startDateTextField, endDateTextField])
+        view.addSubview(datesStackView)
+        
+        datesStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leftAnchor, bottom: nil, trailing: view.rightAnchor, paddingTop: 10, paddingLeft: 20, paddingBottom: 0, paddingRight: 20, width: 0, height: 0, enableInsets: false)
+    }
+    
+    func configureDatePicker() {
+        startDateTextField.inputView = startDatePicker
+        endDateTextField.inputView = endDatePicker
+        startDatePicker.datePickerMode = .date
+        endDatePicker.datePickerMode = .date
+        
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolBar.setItems([flexSpace, doneButton], animated: true)
+        startDateTextField.inputAccessoryView = toolBar
+        endDateTextField.inputAccessoryView = toolBar
+        
+        startDatePicker.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
+        endDatePicker.addTarget(self, action: #selector(dateChanged), for: .valueChanged)
+
+    }
+    
+    @objc func doneAction() {
+        view.endEditing(true)
+    }
+    
+    @objc func dateChanged() {
+        getDateFromPicker()
+    }
+    
+    func getDateFromPicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        startDateTextField.text = formatter.string(from: startDatePicker.date)
+        endDateTextField.text = formatter.string(from: endDatePicker.date)
+
+    }
+
 }
--- ExchangeRatesTestTask/UI/Currency Details/View/CurrencyDetailsViewElements.swift
+++ ExchangeRatesTestTask/UI/Currency Details/View/CurrencyDetailsViewElements.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class CurrencyDetailsViewElements {
 
+    static func createDateTextField() -> UITextField {
+        let dateTextField = UITextField()
+        dateTextField.font = UIFont.systemFont(ofSize: 15)
+        dateTextField.borderStyle = UITextField.BorderStyle.roundedRect
+        dateTextField.autocorrectionType = UITextAutocorrectionType.no
+        dateTextField.clearButtonMode = UITextField.ViewMode.whileEditing
+        dateTextField.contentVerticalAlignment = UIControl.ContentVerticalAlignment.center
+        return dateTextField
+    }
     
+    static func createHorizontalStackView(arrangedSubviews: [UITextField]) -> UIStackView {
+        let stackView = UIStackView(arrangedSubviews: arrangedSubviews)
+        stackView.distribution = .equalSpacing
+        stackView.axis = .horizontal
+        stackView.spacing = 8
+        return stackView
+    }
     
 }
